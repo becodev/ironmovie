d186e917ee5b69f73f65a359ff0d5810724112db
Juan Pablo Loureyro <juanpabloloureyro@gmail.com>
2021-06-17 19:34:57 -0300

    better menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "./components/Footer";
-import Menu from "./components/Menu";
+import Menu from "./components/Menu/Menu";
 import "./index.css";
 
 function App() {
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { AppBar, Toolbar, Slide } from "@material-ui/core";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import logo from "../../assets/ironmovies.png";
+import SearchIcon from "@material-ui/icons/Search";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import InputBase from "@material-ui/core/InputBase";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+function Logo() {
+  return (
+    <>
+      <a href="/">
+        <img src={logo} alt="logo" className="logo" />
+      </a>
+    </>
+  );
+}
+
+const Menu = (props) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <HideOnScroll {...props}>
+        <AppBar color="inherit">
+          <Toolbar>
+            <Logo />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+    </div>
+  );
+};
+
+export default Menu;
